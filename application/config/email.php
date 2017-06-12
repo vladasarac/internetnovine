bb74fbfc273d0da252cad3c65606139f775ca63c
vladasarac <vladasarac@hotmail.com>
2017-06-12 23:10:45 +0200

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -4,5 +4,5 @@ $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_port'] = 465;
 $config['smtp_user'] = 'kantarion35@gmail.com';
-$config['smtp_pass'] = 'pikolopikolo';
+$config['smtp_pass'] = '';
 	  
